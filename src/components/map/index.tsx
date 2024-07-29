a1d5429ae7fe37b7182746162878f7e55cdd9179
XANDOCO MUY LOCO A.K.A EL PERERECO <gxandesss@gmail.com>
2024-07-29 16:21:50 -0300

    points added

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,66 +1,84 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { OSM, Vector as VectorSource } from 'ol/source';
 import { fromLonLat } from 'ol/proj';
-import {Feature, Map, View} from 'ol/index.js';
+import { Feature, Map, View } from 'ol/index.js';
 import { Point } from 'ol/geom';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
-import {useGeographic} from 'ol/proj';
+import { useGeographic } from 'ol/proj';
 
 import styled from 'styled-components';
 import 'ol/ol.css';
 
-import { useGetPlaces } from '../../services/getMaps';
+import PocketBase from 'pocketbase' // Import PocketBase instance
 
 const MapBlock = () => {
-    useGeographic();    
-    const mapContainerRef = useRef(null);
-    const { places, isLoading, isError }  = useGetPlaces();
-
-    console.log(places);
-
-    const mapPoints = !isLoading
-    ? places.map((place) => {
-        const point = new Point([Number(place.long), Number(place.lat)]);
-        console.log([Number(place.long), Number(place.lat)]);
-        return new Feature(point)
+  useGeographic();
+  const mapContainerRef = useRef(null);
+  const [places, setPlaces] = useState([]);
+  const pb = new PocketBase('http://127.0.0.1:8090');
+  useEffect(() => {
+    pb.collection('places').getList(1, 50, {
+        filter: 'created >= "2022-01-01 00:00:00"',
+      }).then((e) => {
+        setPlaces(e.items)
       })
-    : [new Feature(new Point([-48.2772, -18.9146]))];
-    console.log(mapPoints)
-    useEffect(() => {
-        const map = new Map({
-            target: mapContainerRef.current,
-            layers: [
-            new TileLayer({
-                source: new OSM(),
+
+  }, []);
+
+  const coordinates = places.map((place) => {
+    return [Number(place.longitude), Number(place.latitude)]
+  })
+
+  const mapPoints = coordinates.map((coordinate) => {
+    const point = new Point(coordinate);
+    console.log(point)
+    point.on(["onclick"], console.log("click"))
+    return new Feature(point);
+  });
+
+  const centerPoint = coordinates
+
+  useEffect(() => {
+    if (mapPoints.length > 0) {
+      const map = new Map({
+        target: mapContainerRef.current,
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          new VectorLayer({
+            source: new VectorSource({
+              features: mapPoints,
             }),
-            new VectorLayer({
-                source: new VectorSource({
-                    features: mapPoints,
-                }),
-                style: {
-                    'circle-radius': 9,
-                    'circle-fill-color': 'red',
-                },
-                }),
-            ],
-            view: new View({
-            center: [-48.2772, -18.9146],
-            zoom: 12,
-            }),
-        });
+            style: {
+              'circle-radius': 9,
+              'circle-fill-color': 'red',
+            },
+          }),
+        ],
+        view: new View({
+          center: [-48.2772, -18.9146],
+          zoom: 12,
+        }),
+      });
+  
+      return () => {
+        map.setTarget(null);
+      };
+    }
 
-        return () => {
-            map.setTarget(null);
-        };
-    }, [isLoading]);
-    if(isLoading) return <div>Loading...</div>
-    return <Container ref={mapContainerRef} />;
-}
+  }, [places]);
+  console.log(places)
+  if (places.length < 1) {
+    return <h1>loading...</h1>
+  }
+  return <Container ref={mapContainerRef} />;
+};
 
-    const Container = styled.div`
-        width: 100%;
-        height: 600px;
-    `
+const Container = styled.div`
+  width: 100%;
+  height: 600px;
+`;
 
-    export default MapBlock;
+export default MapBlock;
